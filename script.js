2191e42096b6a8b9c08223c33e1985b3d5f55739
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-28 09:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const btn = document.querySelectorAll(".btn");
+const stop = document.querySelector(".stop");
+const audio = document.querySelector("#audio");
+
+btn.forEach((button) => {
+	button.addEventListener('click', () => {
+		const soundName = button.dataset.sound;
+		const soundPath = `sounds/${soundName}.mp3`;
+		audio.src = soundPath;
+		audio.play();
+	});
+});
+
+stop.addEventListener('click', () => {
+	audio.pause();
+	audio.src='';
+});
